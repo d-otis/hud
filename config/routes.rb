e92f005af7918c656cc07a56a27d77d02a3db8cf
d-otis <dan@dan-foley.com>
2021-01-03 14:07:22 -0500

    properly namespace plants route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :plants, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :plants, only: [:index]
+    end
+  end
 end
