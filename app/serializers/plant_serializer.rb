1687110b454991bf034e6d8363d80fd7519d4106
d-otis <dan@dan-foley.com>
2021-01-03 14:22:35 -0500

    correct key spelling error in JSON serializer for Plant

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,4 +1,4 @@
 class PlantSerializer
   include JSONAPI::Serializer
-  attributes :name, :latin_name, :watering, :pest_and_disease, :pruning, :description, :light, :fertilization, :temp_lo, :temp_hi, :room, :soil, :seasons
+  attributes :name, :latin_name, :watering, :pest_and_disease, :pruning, :description, :light, :fertilization, :temp_low, :temp_hi, :room, :soil, :seasons
 end
